123e33808c3b1aec3c9bde0fbe233bb3b3fd36f0
fdlrswlly <fadhly.aulia123@gmail.com>
2023-04-09 10:26:08 +0700

    first commit

---
+++ soal2b.php
@@ -0,0 +1,17 @@
+<?php
+$nama = $_GET['nama'];
+$umur = $_GET['umur'];
+$hobi = $_GET['hobi'];
+?>
+<html>
+    <head>
+        <title>SOAL 2B</title>
+    </head>
+    <body>
+        <?php
+        echo "NAMA: ".$nama."<br>";
+        echo "UMUR: " . $umur . "<br>";
+        echo "HOBI: " . $hobi . "<br>";
+        ?>
+    </body>
+</html>
