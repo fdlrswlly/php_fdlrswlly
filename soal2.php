aa21041a797eae7b01e71e1a2d63f6e83bb36774
fdlrswlly <fadhly.aulia123@gmail.com>
2023-04-09 10:24:21 +0700

    first commit

---
+++ soal2.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>SOAL NO 2</title>
+    </head>
+    <body>
+        <form action="soal2b.php" method="GET">
+            <table>
+                <tr>
+                    <td>NAMA ANDA: </td>
+                    <td>
+                        <input type="text" name="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <td>UMUR ANDA: </td>
+                    <td>
+                        <input type="text" name="umur">
+                    </td>
+                </tr>
+                <tr>
+                    <td>HOBI ANDA: </td>
+                    <td>
+                        <input type="text" name="hobi">
+                    </td>
+                </tr>
+            </table>
+            <input type="submit" name="submit" value="SUBMIT">
+        </form>
+    </body>
+</html>
